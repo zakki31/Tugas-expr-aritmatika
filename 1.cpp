728e5b42e32b48ad4d9c620d3e822622dd1531ca
zakki31 <83547109+zakki31@users.noreply.github.com>
2021-07-11 21:34:53 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,7 +3,7 @@ Anggota Kelompok
 1. Ahmad Muzakki - 2017051037
 2. Indah Nirmala Zahra Nabil - 2057051004
 3. Muhammad Fahmi Bastari - 2057051005
-Link Github : https://github.com/zakki31/TUGAS-PETRUK.git
+Link Github : https://github.com/zakki31/Tugas-expr-aritmatika.git
 */
 
 #include <cmath>
