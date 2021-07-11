e539ec3714d208e73a92c7d774684f01d22c240b
zakki31 <83547109+zakki31@users.noreply.github.com>
2021-07-11 20:28:27 +0700

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,11 @@
+/*
+Anggota Kelompok
+1. Ahmad Muzakki - 2017051037
+2. Indah Nirmala Zahra Nabil - 2057051004
+3. Muhammad Fahmi Bastari - 2057051005
+Link Github : https://github.com/zakki31/TUGAS-PETRUK.git
+*/
+
+/*
+  Buat program untuk mengonversi string infix ke dalam bentuk postfix.
+*/
