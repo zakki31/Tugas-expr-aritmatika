ad3f73cb6008105f76c511e8266b17f2e3414597
zakki31 <83547109+zakki31@users.noreply.github.com>
2021-07-11 21:14:55 +0700

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,8 @@
+/*
+Anggota Kelompok
+1. Ahmad Muzakki - 2017051037
+2. Indah Nirmala Zahra Nabil - 2057051004
+3. Muhammad Fahmi Bastari - 2057051005
+Link Github : https://github.com/zakki31/TUGAS-PETRUK.git
+*/
+
