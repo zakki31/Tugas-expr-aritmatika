83f1f67eac1a9e8d14d130d7da20c8459412926a
zakki31 <83547109+zakki31@users.noreply.github.com>
2021-07-11 21:36:57 +0700

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -9,3 +9,40 @@ Link Github : https://github.com/zakki31/Tugas-expr-aritmatika.git
 /*
   Buat program untuk mengonversi string infix ke dalam bentuk postfix.
 */
+
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string infix, str = "";
+    int len;
+    
+    getline(cin, infix);
+    
+    len = infix.size();
+    
+    for(int i=0; i<len; i++) {
+        if(isalnum(infix[i])) {
+            str += infix[i];
+            if(!(isalnum(infix[i+1])))
+                str += ' ';
+        }
+        else if(infix[i]=='-') {
+            if(isalnum(infix[i+1])) {
+                str += infix[i];
+                if(i!=0)
+                    str += ' ';
+            }
+            else if(infix[i+1]=='(') {
+                if(isalnum(infix[i-1])) {
+                    str += infix[i];
+                    str += ' ';
+                }
+                else {
+                    str += "-1 * ";
+                }
+            }
+            
