d01bb4436acf466432e8a99eeb856c687b5183a0
zakki31 <83547109+zakki31@users.noreply.github.com>
2021-07-11 21:56:03 +0700

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -6,3 +6,59 @@ Anggota Kelompok
 Link Github : https://github.com/zakki31/Tugas-expr-aritmatika.git
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isOperator(char X){
+    if(X == '+' || X == '-' || X == '*' || X == '/' || X == '%')
+    {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int putFirst(string X){
+    if(X == "+" || X == "-")
+	{
+        return 1;
+    }
+    if(X == "*" || X == "/" || X == "%")
+	{
+        return 2;
+    }
+    return 0;
+}
+
+bool large(string X, string Y)
+{
+    int num1, num2;
+    num1 = putFirst(X);
+    num2 = putFirst(Y);
+    return num1 <= num2;
+}
+
+double run (double X, double Y, string n)
+{
+    if(n == "+")
+	{
+        return X+Y;
+    }
+    else if(n == "-")
+	{
+        return X-Y;
+    }
+    else if(n == "*")
+	{
+        return X*Y;
+    }
+    else if(n == "/")
+	{
+        return X/Y;
+    }
+    else if(n == "%")
+	{
+        return (int) X % (int) Y;
+    }
+    return 0;
+}
